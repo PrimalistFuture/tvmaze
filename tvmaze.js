3c40d718d7d5208ef6af30fcc2a4b17efab50098
Vaughn Seekamp <vseekamp@gmail.com>
2023-03-01 15:57:20 -0800

    nearly there with image alt

--- tvmaze.js
+++ tvmaze.js
@@ -19,13 +19,27 @@ const SHOWS_HEADER = 'shows';
 async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
 
-  let response = await axios.get(`${TVMAZE_BASE_URL}` + `${SEARCH_HEADER}` +
-    `${SHOWS_HEADER}`, { params: { q: term } });
+  let response = await axios.get(`${TVMAZE_BASE_URL}search/shows`,
+  { params: { q: term } });
 
   let responseData = response.data;
+  console.log(responseData, "responseData");
+
+  if (responseData.show.show.image === null) {
+    responseData.show.show.image = 'https://tinyurl.com/tv-missing';
+  } else {
+    imageSrc = show.image.original;
+  }
+
+  let showData = responseData.map(data => ({"id": data.show.id,
+  "name": show.show.name, "summary": show.show.summary,
+  "image": show.show.image}))
+
+
+  console.log(showData, "showData");
 
   // Update response data to only include required info (map/loop/w/e)
-  return responseData;
+  return showData;
 }
 
 
@@ -40,22 +54,22 @@ function populateShows(shows) {
 
   console.log('Populate shows called, arrays = ' + shows);
   for (let show of shows) {
-    if (show.show.image === null) {
+    if (show.image === null) {
       imageSrc = 'https://tinyurl.com/tv-missing';
     } else {
-      imageSrc = show.show.image.original;
+      imageSrc = show.image.original;
     }
     console.log(imageSrc)
     const $show = $(
-      `<div data-show-id="${show.show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
               src="${imageSrc}"
               alt="PLACEHOLDER"
               class="w-25 me-3">
            <div class="media-body">
-             <h5 class="text-primary">${show.show.name}</h5>
-             <div><small>${show.show.summary}</small></div>
+             <h5 class="text-primary">${show.name}</h5>
+             <div><small>${show.summary}</small></div>
              <button class="btn btn-outline-light btn-sm Show-getEpisodes">
                Episodes
              </button>
